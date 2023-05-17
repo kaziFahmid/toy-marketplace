3c5dfa01b7e428128cde21d4f74b1801fc72dcce
kaziFahmid <nirjhorislam42@gmail.com>
2023-05-18 00:42:02 +0600

    ssign up has been made

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -6,8 +6,8 @@ export default function Login() {
     <div>
         <h1 className='text-danger text-center mt-4 '>FunToys</h1>
         <p className='text-center mt-2'>Get the most high quality action figures </p>
-            <form className="w-50 mx-auto mb-5 mt-5 ">
-        <div className="shadow p-5">
+            <form className="w-50 mx-auto mb-5 mt-5 shadow ">
+        <div className=" p-5">
           <h2 className="text-center mb-4 text-danger">Login</h2>
           <div className="mb-3">
             <label className="form-label">Email</label>
@@ -21,7 +21,7 @@ export default function Login() {
          <p>You dont have account ?<Link to='/signup'>create an account</Link></p>
 
       <div className='text-center'>
-      <button className="btn btn-light text-black">Google</button><br/>
+    
           <button className="btn btn-danger text-white px-5 mt-3">Login</button>
       </div>
 
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Signup() {
   return (
     <div>
-      
-    </div>
+    <h1 className="text-danger text-center mt-4">FunToys</h1>
+    <p className="text-center mt-2">Get the most high-quality action figures</p>
+  
+    <form className="w-50 mx-auto mb-5 mt-5 shadow p-5">
+        <h2 className='text-danger'>Signup</h2>
+      <div className="form-group mt-3">
+        <label htmlFor="username">Username</label>
+        <input type="text" className="form-control" id="username" name="username" />
+      </div>
+  
+      <div className="form-group mt-3">
+        <label htmlFor="email">Email</label>
+        <input type="email" className="form-control" id="email" name="email" />
+      </div>
+  
+      <div className="form-group mt-3">
+        <label htmlFor="password">Password</label>
+        <input type="password" className="form-control" id="password" name="password" />
+      </div>
+  <p className='mt-2'>Have account <Link to='/login'> Login</Link></p>
+<div className='text-center'>
+<button className=' btn btn-light mt-4 text-black px-5'>Google</button><br/>
+    <button className='text-white btn btn-danger mt-3 px-5'>SignUp</button>
+</div>
+  
+    </form>
+  </div>
+  
   )
 }
